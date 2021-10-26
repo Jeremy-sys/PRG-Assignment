320784012f26c36b045183acee14644b53cf1c4b
Jeremy-sys <72734836+Jeremy-sys@users.noreply.github.com>
2021-10-26 16:00:34 +0200

    Updated the cash distribution module- oneHundredDollars are only to be a max of 50 for security reasons thus a counter keeps track of it and if it is equal to zero then the hunderd dollars are out and the next available cash is diven as change

--- Main.java
+++ Main.java
@@ -1,37 +1,56 @@
 package com.AssignmentCustomer;
+
+
 public class Main {
 
-    public static void main(String[] args) {
-            int amount,oneHundredDollar, tenDollar, fiveDollar, oneDollar, tenCent, fiveCent;
+    public static void changeDistributer (String[] args)
+int amount, oneHundredDollar, maxsize = 50, tenDollar, fiveDollar, oneDollar, tenCent, fiveCent;
 
-       System.out.println("Enter a amount of money you inserted from 200 to .5c:" + args[0]);  //A customer should enter the amount of money they will be inserting in the machine and it will be displayed as well
+            System.out.println("Enter the amount of money for change to be printed:" + args[0]);  //The calculation module should enter the amount of money to be given as change and it will be displayed as well
 
-        //The following code does the calculations of the change and how it should be distributed in terms of the available Money Acceptable
-
+            //The following code does the calculations of the change and how it should be distributed in terms of the available Money Acceptable
             amount = Integer.parseInt(args[0]);
-            oneHundredDollar = amount / 100;
-            amount = amount % 100;
-            tenDollar = amount / 10;
-            amount = amount % 10;
-            fiveDollar = amount / 5;
-            amount = amount % 5;
-            oneDollar = amount / 1;
-            amount = amount % 1;
-            tenCent = amount / 10;
-            amount = amount % 10;
-            fiveCent = amount / 5;
-            amount = amount % 5;
-        System.out.println("Thank you for your support!!");
-        System.out.println("*************************************************\n");
-        System.out.println("You INSERTED: " + args[0]);
-        System.out.println("Your change is disbursed as follows:");
-        System.out.println("N$100X"+oneHundredDollar+", " +"N$10X"+tenDollar+", " +"N$5X"+fiveDollar+ ", " +"N$1"+oneDollar +", "+"10centsX"+tenCent+ ", "+ "10centsX"+fiveCent);
+            if (maxsize >= 0) {
+                oneHundredDollar = amount / 100;
+                amount = amount % 100;
+                tenDollar = amount / 10;
+                amount = amount % 10;
+                fiveDollar = amount / 5;
+                amount = amount % 5;
+                oneDollar = amount / 1;
+                amount = amount % 1;
+                tenCent = amount / 10;
+                amount = amount % 10;
+                fiveCent = amount / 5;
+                amount = amount % 5;
+                System.out.println("Thank you for your support!!");
+                System.out.println("*************************************************\n");
+                System.out.println("You INSERTED: " + args[0]);
+                System.out.println("Your change is disbursed as follows:");
+                System.out.println("N$100X" + oneHundredDollar + ", " + "N$10X" + tenDollar + ", " + "N$5X" + fiveDollar + ", " + "N$1X" + oneDollar + ", " + "10centsX" + tenCent + ", " + "5centsX" + fiveCent);
+         }else if  ( maxsize <= 0 ) {
+                    tenDollar = amount / 10;
+                    amount = amount % 10;
+                    fiveDollar = amount / 5;
+                    amount = amount % 5;
+                    oneDollar = amount / 1;
+                    amount = amount % 1;
+                    tenCent = amount / 10;
+                    amount = amount % 10;
+                    fiveCent = amount / 5;
+                    amount = amount % 5;
+
+                    // After calculation and change available is not available the change will be set for the next value available as change.
+                    System.out.println("Thank you for your support!!");
+                    System.out.println("*************************************************\n");
+                    System.out.println("You INSERTED: " + args[0]);
+                    System.out.println("Your change is disbursed as follows:");
+                    System.out.println("N$100X" + 0 + ", " + "N$10X" + tenDollar + ", " + "N$5X" + fiveDollar + ", " + "N$1X" + oneDollar + ", " + "10centsX" + tenCent + ", " + "5centsX" + fiveCent);
 
 
-
-    }
-    }
-
+                }
+            }
+        }
 /**
  * I hereby acknowledge that the work handed in is my own original work. If I
  * have quoted from any other source this information has been correctly referenced.
